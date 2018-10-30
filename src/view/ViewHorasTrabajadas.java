3775de6a654e26970623cefb04c47b06e61ad5e5
KarT <KarT@DESKTOP-L5QE6FQ>
2018-10-30 20:09:10 -0300

    Horas Trabajadas con nuevos textbox

--- src/view/ViewHorasTrabajadas.java
+++ src/view/ViewHorasTrabajadas.java
@@ -19,6 +19,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import interfaces.SistemaPresentismo;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class ViewHorasTrabajadas extends JFrame {
 
@@ -28,6 +30,18 @@ public class ViewHorasTrabajadas extends JFrame {
 	private DefaultTableModel modeloTabla;
 	private Vector<Vector<String>> datosTabla;
 	private Vector<String> columnNames;
+	private JLabel lblCuitcuil;
+	private JLabel lblFechaInicio;
+	private JLabel lblFechaFin;
+	private JTextField textField_CUITCUIL;
+	private JTextField textField_FI_DD;
+	private JTextField textField_FI_AAAA;
+	private JTextField textField_FI_MM;
+	private JLabel label;
+	private JLabel label_1;
+	private JTextField textField_FF_DD;
+	private JTextField textField_FF_MM;
+	private JTextField textField_FF_AAAA;
 	
 	/**
 	 * Launch the application.
@@ -48,6 +62,7 @@ public class ViewHorasTrabajadas extends JFrame {
 	
 	public ViewHorasTrabajadas()
 	{
+		setTitle("Horas Trabajadas");
 		iniciarVentasAnteriores();
 	}
 	
@@ -75,43 +90,96 @@ public class ViewHorasTrabajadas extends JFrame {
 	 */
 	private void iniciarVentasAnteriores() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 557, 347);
+		setBounds(100, 100, 557, 472);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(15, 169, 515, 209);
 		
 		JButton btnVolver = new JButton("Volver");
+		btnVolver.setBounds(468, 389, 63, 23);
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0)
 			{
 				dispose();
 			}
 		});
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnVolver)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 515, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
-					.addComponent(btnVolver))
-		);
+		
+		lblCuitcuil = new JLabel("CUIT/CUIL");
+		lblCuitcuil.setBounds(15, 16, 94, 22);
+		
+		lblFechaInicio = new JLabel("Fecha Inicio");
+		lblFechaInicio.setBounds(15, 51, 100, 22);
+		
+		lblFechaFin = new JLabel("Fecha Fin");
+		lblFechaFin.setBounds(15, 87, 112, 22);
+		
+		textField_CUITCUIL = new JTextField();
+		textField_CUITCUIL.setBounds(133, 17, 148, 20);
+		textField_CUITCUIL.setColumns(10);
+		
+		textField_FI_DD = new JTextField();
+		textField_FI_DD.setBounds(133, 52, 35, 21);
+		textField_FI_DD.setColumns(10);
+		
+		textField_FI_AAAA = new JTextField();
+		textField_FI_AAAA.setBounds(253, 52, 46, 20);
+		textField_FI_AAAA.setColumns(10);
+		
+		textField_FI_MM = new JTextField();
+		textField_FI_MM.setBounds(191, 52, 35, 20);
+		textField_FI_MM.setColumns(10);
+		
+		label = new JLabel("/");
+		label.setBounds(172, 49, 15, 27);
+		
+		label_1 = new JLabel("/");
+		label_1.setBounds(234, 49, 15, 27);
+		
+		textField_FF_DD = new JTextField();
+		textField_FF_DD.setBounds(131, 90, 35, 21);
+		textField_FF_DD.setColumns(10);
+		
+		JLabel label_2 = new JLabel("/");
+		label_2.setBounds(170, 87, 15, 27);
+		
+		textField_FF_MM = new JTextField();
+		textField_FF_MM.setBounds(189, 90, 35, 20);
+		textField_FF_MM.setColumns(10);
+		
+		JLabel label_3 = new JLabel("/");
+		label_3.setBounds(232, 87, 15, 27);
+		
+		textField_FF_AAAA = new JTextField();
+		textField_FF_AAAA.setBounds(251, 90, 46, 20);
+		textField_FF_AAAA.setColumns(10);
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		contentPane.setLayout(gl_contentPane);
 		table.setModel(new DefaultTableModel(datosTabla, columnNames));
+		contentPane.setLayout(null);
+		contentPane.add(btnVolver);
+		contentPane.add(scrollPane);
+		contentPane.add(lblFechaFin);
+		contentPane.add(textField_FF_DD);
+		contentPane.add(label_2);
+		contentPane.add(textField_FF_MM);
+		contentPane.add(label_3);
+		contentPane.add(textField_FF_AAAA);
+		contentPane.add(lblFechaInicio);
+		contentPane.add(lblCuitcuil);
+		contentPane.add(textField_CUITCUIL);
+		contentPane.add(textField_FI_DD);
+		contentPane.add(label);
+		contentPane.add(textField_FI_MM);
+		contentPane.add(label_1);
+		contentPane.add(textField_FI_AAAA);
+		
+		JButton btnListar = new JButton("Listar");
+		btnListar.setBounds(290, 121, 112, 23);
+		contentPane.add(btnListar);
 		modeloTabla = (DefaultTableModel) table.getModel();
 	    modeloTabla.setDataVector(datosTabla, columnNames);
 	}
