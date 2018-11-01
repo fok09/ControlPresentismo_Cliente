a4b63617daa2cffe4238ffab29a527678ad13bf7
KarT <KarT@DESKTOP-L5QE6FQ>
2018-11-01 16:32:38 -0300

    new view

---
+++ src/view/ViewEnviarReporteFichadas.java
@@ -0,0 +1,251 @@
+package view;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import bean.Cliente;
+import bean.Contratacion;
+import bean.PersonaJuridica;
+import interfaces.SistemaPresentismo;
+import srv.ClienteSrv;
+
+public class ViewEnviarReporteFichadas extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private SistemaPresentismo controlPresentismo;
+	private DefaultTableModel modeloTabla;
+	private Vector<Vector<String>> datosTabla;
+	private Vector<String> columnNames;
+	private List<Contratacion> contrataciones;
+	private Date cFechaInicio;
+	private Date cFechaFin;
+	private JLabel lblCuitcuil;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewEnviarReporteFichadas frame = new ViewEnviarReporteFichadas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public boolean getStub() {
+    	
+    	try {
+    		controlPresentismo = (SistemaPresentismo)Naming.lookup ("//localhost/ControladorPresentismo");
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+    }
+	
+	public void actualizarTabla()
+	{
+		modeloTabla = (DefaultTableModel) table.getModel();
+		modeloTabla.setDataVector(datosTabla, columnNames);
+	}
+	
+	public ViewEnviarReporteFichadas()
+	{
+		setTitle("Envio de Reporte");
+		columnNames = new Vector<String>();
+		//columnNames.addElement("Cuit/Cuil");
+		//columnNames.addElement("DNI");
+	    columnNames.addElement("Legajo");
+	    columnNames.addElement("Nombre");
+	    columnNames.addElement("Total horas");
+	    columnNames.addElement("Horas Ausente");
+	    //columnNames.addElement("Horas de Vacaciones");
+		iniciarPantalla();
+	}
+	
+//	public ViewHorasTrabajadas() throws RemoteException
+//	{
+//		String cuit_cuil = "";
+//		Date fechaInicio = new Date;
+//		Date fechaFin;
+		
+//	    columnNames.addElement("Horas Ausente");
+//		iniciarVentasAnteriores();
+//	}
+	
+	
+	
+
+	/**
+	 * Create the frame.
+	 */
+	private void iniciarPantalla() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 565, 496);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(15, 122, 515, 228);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setBounds(423, 421, 112, 23);
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+				dispose();
+			}
+		});
+		
+		lblCuitcuil = new JLabel("CUIT/CUIL");
+		lblCuitcuil.setBounds(15, 16, 94, 22);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(datosTabla, columnNames));
+		contentPane.setLayout(null);
+		contentPane.add(btnVolver);
+		contentPane.add(scrollPane);
+		contentPane.add(lblCuitcuil);
+		
+		JComboBox comboBox_Contratacion = new JComboBox();
+		comboBox_Contratacion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String seleccion = comboBox_Contratacion.getSelectedItem().toString();
+				int cod = Integer.parseInt(seleccion.substring(0,seleccion.indexOf(" ")));
+				for (Contratacion c : contrataciones ) {
+					if (c.getId()==cod) {
+						cFechaInicio = c.getFechaInicial();
+						cFechaFin = c.getFechaFinal();
+					}
+				}
+	
+				
+			}
+		});
+		comboBox_Contratacion.setBounds(131, 50, 362, 21);
+		contentPane.add(comboBox_Contratacion);
+		
+		JComboBox comboBox_Empresa = new JComboBox();
+		comboBox_Empresa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				if (getStub()) {
+					
+					try {
+						String cuitEmpresa = (String) comboBox_Empresa.getSelectedItem().toString();
+						cuitEmpresa = cuitEmpresa.substring(0,cuitEmpresa.indexOf(" "));
+						
+						contrataciones = controlPresentismo.getContratacionesCliente(cuitEmpresa);
+						comboBox_Contratacion.removeAllItems();
+						for (Contratacion c : contrataciones){
+							comboBox_Contratacion.addItem(c.getId() + " " + c.getServicio().getNombre() + " - " + c.getFechaInicial() + " a " + c.getFechaFinal());							
+						}
+					} catch (RemoteException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+				}
+			}
+			
+		});
+		if(getStub()){
+			List<PersonaJuridica> clientesPJ;
+			clientesPJ=ClienteSrv.getClientesJuridicos();
+			for (int i=0; i<clientesPJ.size();i++){
+				comboBox_Empresa.addItem(clientesPJ.get(i).getCuit_cuil().toString() + " - " + clientesPJ.get(i).getRazonSocial());
+			}
+		}
+		comboBox_Empresa.setBounds(131, 17, 362, 21);
+		contentPane.add(comboBox_Empresa);
+		
+		
+		JButton btnListar = new JButton("Listar");
+		btnListar.setBounds(418, 82, 112, 23);
+		btnListar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+				if (getStub()) {
+					try {
+						Contratacion cont = new Contratacion();
+						String cuit = (String) comboBox_Empresa.getSelectedItem();
+						String seleccion = comboBox_Contratacion.getSelectedItem().toString();
+						int cod = Integer.parseInt(seleccion.substring(0,seleccion.indexOf(" ")));
+						for (Contratacion c : contrataciones ) {
+							if (c.getId()==cod) {
+								cont = c;
+							}
+						}
+						
+						datosTabla = controlPresentismo.getHorasTrabajadasTotales(cuit,cFechaInicio,cFechaFin, cont);
+						actualizarTabla();
+					} catch (RemoteException e1) {
+						e1.printStackTrace();
+					}
+					}
+			}
+		});
+		
+		contentPane.add(btnListar);
+		
+		
+		
+		JLabel lblContratacion = new JLabel("Contratacion");
+		lblContratacion.setBounds(15, 49, 94, 22);
+		contentPane.add(lblContratacion);
+		
+		JLabel lblEnviarReporteA = new JLabel("Enviar reporte a");
+		lblEnviarReporteA.setBounds(15, 383, 94, 19);
+		contentPane.add(lblEnviarReporteA);
+		
+		JComboBox comboBox_Destinatario = new JComboBox();
+		comboBox_Destinatario.setBounds(136, 382, 208, 21);
+		contentPane.add(comboBox_Destinatario);
+		comboBox_Destinatario.addItem("Gimnasio");
+		comboBox_Destinatario.addItem("Liquidacion de Sueldos");
+		
+		JButton btnNewButton = new JButton("Enviar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (comboBox_Destinatario.getSelectedItem().toString().equals("Gimnasio")){
+					//Agregar Integracion con Gym
+				}
+				if (comboBox_Destinatario.getSelectedItem().toString().equals("Liquidacion de Sueldos")){
+					//Agregar PostLiquidacion
+				}
+			}
+		});
+		btnNewButton.setBounds(423, 379, 112, 23);
+		contentPane.add(btnNewButton);
+
+		
+		modeloTabla = (DefaultTableModel) table.getModel();
+	    modeloTabla.setDataVector(datosTabla, columnNames);
+	}
+}
