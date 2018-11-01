219d0bfae2b4c0a4f1a2c735f2884c1b4d25e099
sofia <sofia@192.168.208.124>
2018-11-01 19:39:58 -0300

    view fichada

--- src/view/ViewCrearFichada.java
+++ src/view/ViewCrearFichada.java
@@ -115,7 +115,6 @@ public class ViewCrearFichada extends JFrame {
 			for (int i=0; i<clientes.size();i++){
 				comboBox_Empresa.addItem(clientes.get(i).getCuit_cuil());
 			}
-			System.out.println(clientes.size());
 		}
 		
 		JButton btnNewButton = new JButton("Fichar");
@@ -125,7 +124,6 @@ public class ViewCrearFichada extends JFrame {
 					try {
 						LocalDateTime hora = LocalDateTime.now();
 						Date fecha = Calendar.getInstance().getTime();
-						System.out.println("prueba");
 						List<Cliente> clientes;
 						clientes=ClienteSrv.getClientes();
 						Empleado emp= new Empleado();
